3f9aca6f2bba7acc5624640fb35fe9ff87ee9927
costarowan-g <genevieve.rowan@gmail.com>
2024-10-18 15:17:24 +0100

    Update to all weather data included

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 function updateTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
+  let description = response.data.condition.description;
+  let humidity = response.data.temperature.humidity;
+  let windSpeed = response.data.wind.speed;
+  let date = new Date(response.data.time * 1000);
   let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = response.data.city;
   let temperatureElement = document.querySelector("#weather-app-temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time-date");
+
+  console.log(response);
+
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = description;
+  humidityElement.innerHTML = humidity;
+  windSpeedElement.innerHTML = windSpeed;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
