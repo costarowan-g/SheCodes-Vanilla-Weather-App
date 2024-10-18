b9734f95cd41ef961598c8ee3d95fed6f8ef91f8
costarowan-g <genevieve.rowan@gmail.com>
2024-10-18 14:46:34 +0100

    Axios and temperature update added
    Axios included, api added.
    Temperature update function included using api
    Load page default set to Lisbon.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "ab7t243ba2b8a25f4fb421dc30o50328";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(updateTemperature);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Lisbon");
